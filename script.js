f4202e04e2ebf0113d135c2f568f09f81b47b601
Hossam <jkasdhkjadas>
2017-01-25 20:59:19 +0200

    Adding Styles

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 (function() {
 	"use strict";
-	var titles = [];
-	var links;
-	var sentences;
 	var callback = function(data) {
 		for (var i = 0;i < data.query.search.length;i++) {
-			titles[i] = data.query.search[i].title;
 			$.ajax({
 				url:"https://en.wikipedia.org/w/api.php?",
 				dataType:"jsonp",
 				format:"json",
-				data:"action=query&format=json&prop=extracts&list=&titles="+ titles[i] +"&exsentences=1&exlimit=10&exintro=1&explaintext=1",
+				data:"action=query&format=json&prop=extracts&list=&titles="+ data.query.search[i].title +"&exsentences=1&exlimit=10&exintro=1&explaintext=2",
 				success:function(data) {
 					for (var k in data.query.pages) {
-						links= "https://en.wikipedia.org/?curid=" + k;
-						sentences = data.query.pages[k].extract;
-						console.log(sentences);
+						document.getElementById("pages").innerHTML += "<div class='entry'><a href = 'https://en.wikipedia.org/?curid=" + k + "'> <h2>" + data.query.pages[k].title + "</h2> " + data.query.pages[k].extract + "</a></div>";
 						break;
 					}
-					$("")
 				}
-			})
+			});
 		}
 	};
 	$.ajax({
