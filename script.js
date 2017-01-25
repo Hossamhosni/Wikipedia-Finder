b99b9cf070c7803f082c9b122659801a4fffacf1
Hossam <jkasdhkjadas>
2017-01-26 01:55:13 +0200

    Removed autocompelte bug

--- script.js
+++ script.js
@@ -1,51 +1,6 @@
 (function() {
 	"use strict";
-	var callback = function(data) {
-		document.getElementById("pages").innerHTML = "";
-		document.getElementById("autocomplete").innerHTML = "";
 
-		for (var i = 0;i < data.query.search.length;i++) {
-			$.ajax({
-				url:"https://en.wikipedia.org/w/api.php?",
-				dataType:"jsonp",
-				format:"json",
-				data:"action=query&format=json&prop=extracts&list=&titles="+ data.query.search[i].title +"&exsentences=1&exlimit=10&exintro=1&explaintext=2",
-				success:function(data) {
-					for (var k in data.query.pages) {
-						document.getElementById("pages").innerHTML += "<div class='entry'><a href = 'https://en.wikipedia.org/?curid=" + k + "'> <h2>" + data.query.pages[k].title + "</h2> " + data.query.pages[k].extract + "</a></div>";
-						$("#pages").fadeIn();
-						break;
-					}
-				}
-			});
-		}
-	};
-	var callback2 = function(data) {
-		document.getElementById("autocomplete").innerHTML = "";
-		if ($("#searchBar").val() == ""){
-			return;
-		}
-		try {
-			for (var i = 0;i < data.query.search.length;i++) {
-				$.ajax({
-					url:"https://en.wikipedia.org/w/api.php?",
-					dataType:"jsonp",
-					format:"json",
-					data:"action=query&format=json&list=&titles="+ data.query.search[i].title,
-					success:function(data) {
-						for (var k in data.query.pages) {
-							document.getElementById("autocomplete").innerHTML += "<div class='suggestions'><a href = 'https://en.wikipedia.org/?curid=" + k + "'> <h4>" + data.query.pages[k].title + "</h4> </a></div>";
-							break;
-						}
-					}
-				});
-			}
-		}
-		catch (e) {
-			console.log(e);
-			return;
-		}
-	};
 	$("#clear").on("click",function(){
 		$("#searchBar").val("");
 		document.getElementById("autocomplete").innerHTML = "";
@@ -55,7 +10,7 @@
 		document.getElementById("searchBar").focus();
 		$("#searchBar").on('keyup',function(e) {
 			var entry = $("#searchBar").val();
-			$("#pages").fadeOut();
+			//$("#pages").fadeOut();
 			if($("#searchBar").val() != "" && e.keyCode == 13){
 				document.getElementById("autocomplete").innerHTML = "";
 				$("#autocomplete").css("display","none");
@@ -78,4 +33,53 @@
 			}
 		});
 	});
+	var callback = function(data) {
+		$("#pages").css("display","none");
+		document.getElementById("pages").innerHTML = "";
+		document.getElementById("autocomplete").innerHTML = "";
+
+		for (var i = 0;i < data.query.search.length;i++) {
+			$.ajax({
+				url:"https://en.wikipedia.org/w/api.php?",
+				dataType:"jsonp",
+				format:"json",
+				data:"action=query&format=json&prop=extracts&list=&titles="+ data.query.search[i].title +"&exsentences=1&exlimit=10&exintro=1&explaintext=2",
+				success:function(data) {
+					for (var k in data.query.pages) {
+						document.getElementById("pages").innerHTML += "<div class='entry'><a href = 'https://en.wikipedia.org/?curid=" + k + "'> <h2>" + data.query.pages[k].title + "</h2> " + data.query.pages[k].extract + "</a></div>";
+						$("#pages").fadeIn("slow");
+						break;
+					}
+				}
+			});
+		}
+	};
+	var callback2 = function(data) {
+
+		document.getElementById("autocomplete").innerHTML = "";
+		if ($("#searchBar").val() == ""){
+			return;
+		}
+		try {
+
+			for (var i = 0;i < data.query.search.length;i++) {
+				$.ajax({
+					url:"https://en.wikipedia.org/w/api.php?",
+					dataType:"jsonp",
+					format:"json",
+					data:"action=query&format=json&list=&titles="+ data.query.search[i].title,
+					success:function(data) {
+						for (var k in data.query.pages) {
+							document.getElementById("autocomplete").innerHTML += "<div class='suggestions'><a href = 'https://en.wikipedia.org/?curid=" + k + "'> <h4>" + data.query.pages[k].title + "</h4> </a></div>";
+							$("#autocomplete").css("display","block");
+							break;
+						}
+					}
+				});
+			}
+		}
+		catch (e) {
+			return;
+		}
+	};
 }());
