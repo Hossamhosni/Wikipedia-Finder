37557cd6735417bf3046d684a1a51a6a0fd72034
Hossam <jkasdhkjadas>
2017-01-26 00:59:43 +0200

    Added autocomplete and changed style

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 (function() {
 	"use strict";
 	var callback = function(data) {
+		document.getElementById("pages").innerHTML = "";
+		document.getElementById("autocomplete").innerHTML = "";
+
 		for (var i = 0;i < data.query.search.length;i++) {
 			$.ajax({
 				url:"https://en.wikipedia.org/w/api.php?",
@@ -10,18 +13,69 @@
 				success:function(data) {
 					for (var k in data.query.pages) {
 						document.getElementById("pages").innerHTML += "<div class='entry'><a href = 'https://en.wikipedia.org/?curid=" + k + "'> <h2>" + data.query.pages[k].title + "</h2> " + data.query.pages[k].extract + "</a></div>";
+						$("#pages").fadeIn();
 						break;
 					}
 				}
 			});
 		}
 	};
-	$.ajax({
-		url:"https://en.wikipedia.org/w/api.php?",
-		dataType:"jsonp",
-		format:"json",
-		data:"action=query&format=json&prop=&list=search&indexpageids=1&titles=&srsearch=batman&srnamespace=0&srlimit=10&srqiprofile=classic_noboostlinks&srprop=sectionsnippet",
-		success:callback
+	var callback2 = function(data) {
+		document.getElementById("autocomplete").innerHTML = "";
+		if ($("#searchBar").val() == ""){
+			return;
+		}
+		try {
+			for (var i = 0;i < data.query.search.length;i++) {
+				$.ajax({
+					url:"https://en.wikipedia.org/w/api.php?",
+					dataType:"jsonp",
+					format:"json",
+					data:"action=query&format=json&list=&titles="+ data.query.search[i].title,
+					success:function(data) {
+						for (var k in data.query.pages) {
+							document.getElementById("autocomplete").innerHTML += "<div class='suggestions'><a href = 'https://en.wikipedia.org/?curid=" + k + "'> <h4>" + data.query.pages[k].title + "</h4> </a></div>";
+							break;
+						}
+					}
+				});
+			}
+		}
+		catch (e) {
+			console.log(e);
+			return;
+		}
+	};
+	$("#clear").on("click",function(){
+		$("#searchBar").val("");
+		document.getElementById("autocomplete").innerHTML = "";
+		document.getElementById("searchBar").focus();
 	});
+	$(document).ready(function() {
+		document.getElementById("searchBar").focus();
+		$("#searchBar").on('keyup',function(e) {
+			var entry = $("#searchBar").val();
+			$("#pages").fadeOut();
+			if($("#searchBar").val() != "" && e.keyCode == 13){
+				document.getElementById("autocomplete").innerHTML = "";
+				$("#autocomplete").css("display","none");
+				$.ajax({
+					url:"https://en.wikipedia.org/w/api.php?",
+					dataType:"jsonp",
+					format:"json",
+					data:"action=query&format=json&prop=&list=search&indexpageids=1&titles=&srsearch="+entry+"&srnamespace=0&srlimit=10&srqiprofile=classic_noboostlinks&srprop=sectionsnippet",
+					success:callback
+				});
+			} else  {
 
-}())
+				$.ajax({
+					url:"https://en.wikipedia.org/w/api.php?",
+					dataType:"jsonp",
+					format:"json",
+					data:"action=query&format=json&prop=&list=search&indexpageids=1&titles=&srsearch="+entry+"&srnamespace=0&srlimit=4&srqiprofile=classic_noboostlinks&srprop=sectionsnippet",
+					success:callback2
+				});
+			}
+		});
+	});
+}());
